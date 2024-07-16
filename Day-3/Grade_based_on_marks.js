10b7f1421d10ecbdb6d37ff474ce681268d13173
Shubham Mishra <131137726+shubhammishra9039@users.noreply.github.com>
2024-07-16 08:24:08 +0530

    Done

---
+++ Day-3/Grade_based_on_marks.js
@@ -0,0 +1,18 @@
+var a =Number(prompt("Enter A Number :"));
+if(a>90)
+{
+    document.write(`you has Fist Division A++ pass ${a}`)
+    
+}
+else if (a>80)
+{
+    document.write(`you has Fist Division A+ pass ${a}`)
+}
+else if (a>50)
+{
+    document.write(`you has Seconde  Division B+ pass ${a}`)
+}
+else
+{
+    document.write(`you has  C Flied ${a}`)
+}
