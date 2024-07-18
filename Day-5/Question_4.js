8be8fc5bc0d6db8e3d1defedc3b2ebdf5bc5064a
Shubham Mishra <131137726+shubhammishra9039@users.noreply.github.com>
2024-07-18 08:32:58 +0530

    Done

---
+++ Day-5/Question_4.js
@@ -0,0 +1,15 @@
+// ### Question 4
+// concatenation
+// - Add 20 to your name and check the value in the console.
+// - Add true to your name and check the output.
+// - Add 30 + "learnjavascript" and check the type of the final value.
+
+var a = 20 + "Shubham";
+var b = true + "Shubham";
+var c = 30 + "javaScript";
+console.log(a);
+console.log(typeof a);
+
+console.log(b);
+console.log(typeof b);
+console.log(typeof c);
